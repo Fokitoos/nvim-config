dcf443d81e1e9784a7d25d30721406ddabf66e5b
Yannis F <fokito@pop-os.home>
2026-02-01 11:51:29 +0100

    add latest config

---
+++ init.lua
@@ -0,0 +1,5 @@
+require("keymaps")
+require("config.lazy")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
---
+++ lua/plugins/conform.lua
@@ -0,0 +1,31 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    })
+
+    -- Keymaps
+    vim.keymap.set("n", "<leader>gf", function()
+      conform.format({ async = true, lsp_fallback = true })
+    end, { noremap = true, silent = true, desc = "Format buffer" })
+
+    -- Optional: Format on save
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*",
+      callback = function(args)
+        conform.format({ bufnr = args.buf })
+      end,
+    })
+  end,
+}
